1805184df599f4b332e61805d6aee3803f240e05
grillgrut <grillgrut@gmail.com>
2026-05-21 13:07:00 -0500

    A simple while loop and an if statement

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,21 @@
-const repeatString = function() {
+
+let name = "hello";
+let num = 0
+
+const repeatString = function(name, num) {
+
+    if (num < 0) {
+        return "ERROR";
+    }
+
+
+    let words = '';
+
+    for (let i = 0; i < num; i++) {
+        words += name;
+    }
+
+    return words;
 
 };
 
