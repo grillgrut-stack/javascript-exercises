947682d700a1f4e176149307c81a2730a308d0ce
grillgrut <grillgrut@gmail.com>
2026-05-24 11:27:39 -0500

    Map the string to an array then add the characters to an empty string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,17 @@
-const reverseString = function() {
+let name = "Hello"
+const reverseString = function(name) {
+
+    let reverse = "";
+
+    let chars = Array.from(name);
+
+    
+    for (let i = name.length - 1 ; i >= 0; i--) {
+        reverse += chars[i];
+    }
+
+
+    return reverse;
 
 };
 
