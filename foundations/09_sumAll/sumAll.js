c8754fc2a1979d77bbcfbfa0e19786016b45fc32
grillgrut <grillgrut@gmail.com>
2026-05-25 10:21:19 -0500

    Use a for loop to add all the values, then add two checks to ensure that the value is an int. Also, add a check to make sure that is uses the greater value when adding so it does not get stuck.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
 
+
+
+
+const sumAll = function(numOne, numTwo) {
+
+    let sum = 0;
+
+    if (numOne > 0 && numTwo > 0 && Number.isInteger(numOne) && Number.isInteger(numTwo) ) {
+
+    if (numOne < numTwo) {
+        for (let i = numOne; i <= numTwo; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = numOne; i >= numTwo; i--) {
+            sum += i;
+        }
+    }
+
+    } else {
+        return "ERROR";
+    }
+
+
+    return sum;
 };
 
 // Do not edit below this line
