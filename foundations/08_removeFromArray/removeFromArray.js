5326c6c7470e9bca860c5ad3558b229d40df2c31
grillgrut <grillgrut@gmail.com>
2026-05-25 10:04:19 -0500

    Use a nested loop to move through both arrays, and check if they are equal then remove them. Remove them using the splice function for arrays.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
-const removeFromArray = function() {
+let arr = [1, 2, 3, 4];
+
+let num = 3
+
+const removeFromArray = function(arr, ...removeNum) {
+
+   
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let z = 0; z < removeNum.length; z++) {
+            if (arr[i] === removeNum[z]) {
+                    arr.splice(i,1);
+                    i--;
+            }
+
+        }
+    }
+
+    return arr;
+
 };
 
 // Do not edit below this line
