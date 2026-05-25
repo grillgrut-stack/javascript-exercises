b67b64ced1025ed426f6e1af5029905738152c7a
grillgrut <grillgrut@gmail.com>
2026-05-25 10:43:12 -0500

    This is 3 checks that ensure the year is a leap year. The important thing is the order that the checks happen.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(numYear) {
+    if((numYear % 4) === 0) {
+        if((numYear % 400) === 0) {
+            return true;
+        }
 
+        if((numYear % 100) === 0) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+    return false;
 };
 
 // Do not edit below this line
