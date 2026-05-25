e183b2c7b73a492f290de239b732b8a8627698e3
grillgrut <grillgrut@gmail.com>
2026-05-25 11:02:28 -0500

    Simple function, just ensure that you use the right formula for each function. Also, make use of Math.round for the rounding!

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempCel) {
+    tempCel = ((tempCel - 32) * (5/9));
+    tempCel = Math.round(tempCel * 10) / 10;
+
+    return tempCel;
 };
 
-const convertToFahrenheit = function() {
+
+
+const convertToFahrenheit = function(tempFahr) {
+    tempFahr = (tempFahr * (9/5) + 32);
+    tempFahr = Math.round(tempFahr * 10) / 10;
+
+    return tempFahr;
 };
 
 // Do not edit below this line
